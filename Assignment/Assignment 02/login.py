be90487efd016e8006765bf8e6c3a4dac6ea9196
Jason <D23090120503@cityu.edu.mo>
2025-02-17 16:53:27 +0800

    Assignment 02

--- Assignment/Assignment 02/login.py
+++ Assignment/Assignment 02/login.py
@@ -1,11 +1,12 @@
 '''
 Author: Jason Shi
-Date: 14-02-2025 14:49:10
 Last Editors: Jason
 Contact Last Editors: D23090120503@cityu.edu.mo
-LastEditTime: 14-02-2025 15:04:57
+LastEditTime: 17-02-2025 02:03:58
 '''
+
 from tkinter import *
+import hashlib
 
 # root window
 root = Tk()
@@ -27,6 +28,32 @@ password_entry.place(relx=0.5, rely=0.4, anchor=CENTER)
 # login button
 login_button = Button(root, text='登录')
 login_button.place(relx=0.5, rely=0.5, anchor=CENTER)
+# Create a single persistent label for showing messages
+message_label = Label(root, text='')
+message_label.place(relx=0.5, rely=0.6, anchor=CENTER)
+
+# login function
+
+
+def login():
+    name = ID_entry.get()
+    pwd = password_entry.get()
+    input_password = hashlib.sha256(pwd.encode()).hexdigest()
+    password_hash = "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918"
+    global count
+    if name == 'root' and input_password == password_hash:
+        message_label.config(text='登录成功', fg='green')
+        login_button.config(state=DISABLED)
+    else:
+        count += 1
+        if count >= 3:
+            message_label.config(text='错误次数过多，拒绝访问', fg='red')
+            login_button.config(state=DISABLED)
+        else:
+            message_label.config(text='用户名或密码错误', fg='red')
+    return count
+
+
+count = 0
+login_button.config(command=login)
 root.mainloop()
-
-
