108b3b5c1bd26830352ce44c6ac1fa72b22ae0c9
Jason <D23090120503@cityu.edu.mo>
2025-03-24 21:19:53 +0800

    assignment

--- Assignment/Assignment 02/pwd.py
+++ Assignment/Assignment 02/pwd.py
@@ -1,3 +1,12 @@
+'''
+Author: Jason Shi
+Date: 25-02-2025 17:31:00
+LastEditors: Jason
+Contact Last Editors: D23090120503@cityu.edu.mo
+LastEditTime: 25-02-2025 20:44:30
+'''
+
+
 import hashlib
 
 print(hashlib.sha256("admin".encode()).hexdigest())
